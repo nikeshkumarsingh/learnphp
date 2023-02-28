51ad5566a357c4c873aac96f030c3eed419771a3
nikeshkumar <nikeshmasai1947@gmail.com>
2023-02-28 13:26:46 +0530

    learn delearation of variable

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My name is Nikesh</h1>
+    <?php
+    //  $name= "nikesh";
+     $arr = array(12,15,75,54);
+     echo var_dump($arr);
+    //  echo "<h2>".$name."</h2>";
+    ?>
+</body>
+</html>
